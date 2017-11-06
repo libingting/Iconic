0113781b554542bbbb73d7aa3bf2a1e75f20d6ad
libingting <lbti@qq.com>
2017-11-06 17:21:34 +0800

    按时

--- Source/Iconizer.sh
+++ Source/Iconizer.sh
@@ -6,88 +6,57 @@
 #  Created by Ignacio Romero on 5/28/16.
 #  Copyright © 2016 DZN Labs All rights reserved.
 #
-# Script in charge of executing SwitfGen, passing the icon font file path, the enum name and the custom stencil as arguments.
-#
 
-# The optional font file path passed as arg
-INPUT_PATH=$1
+# File/Directory paths
+EXEC_PATH=./Vendor/SwiftGen/build/swiftgen/bin/swiftgen
+STENCIL_PATH=./Source/Iconizer/iconic-default.stencil
+HTML_DIR=./Source/Iconizer/catalog/
 
-# The root path for the generated files
-OUTPUT_PATH=Source
+# Font file path
+FILE_PATH=$1
 
-# The font catalog output path
-CATALOG_PATH=Source/Catalog
-
-# The path of custom Iconic stencil
-STENCIL_PATH=Source/iconic-default.stencil
-
-# The path of SwiftGen exec
-EXEC_PATH=Vendor/SwiftGen/build/swiftgen/bin/swiftgen
-
-
-function getFileTitle()
-{
-# Input variables
-FILE_NAME=$1
-
-# Removes the file extension
-name="${FILE_NAME%.*}"
-
-# Splits and removes all substrings with the separator characters in the file name
-# like whitespaces, dash, underscore, etc.
-title="${name%%[" -_–"]*}"
-
-# Specially, we want to strip the string starting from the word "Icon", and append it manually,
-# so a string like 'MaterialIconsFont' would look like 'MaterialIcon'.
-title="${title%%"icon"*}"
-title="${title%%"Icon"*}Icon"
-
-# Upper case the first character
-title="$(tr '[:lower:]' '[:upper:]' <<< ${title:0:1})${title:1}"
-echo "${title}"
-}
-function iconize()
-{
-# Input variables
-FONT_PATH=$1
-FONT_NAME=$2
-# Capitalized first word of the file name, with the 'Icon' suffix.
-# ie: FontAwesomeIcon out of a string like 'FontAwesome'
-OUTPUT_NAME=$( getFileTitle "${FONT_NAME}" )
-echo "Iconizer: Generating API name '${OUTPUT_NAME}'"
-# Creates the output folder (no error if existing)
-mkdir -p ${OUTPUT_PATH}/
-# Executes Swiftgen with a custom stencil template
-${EXEC_PATH} icons ${FONT_PATH} --templatePath ${STENCIL_PATH} --output ${OUTPUT_PATH}/${OUTPUT_NAME}.swift --enumName ${OUTPUT_NAME}
-# Moves and renames the JSON output to the HTML directory
-mv ${OUTPUT_PATH}/${OUTPUT_NAME}.json ${CATALOG_PATH}/data.json
-echo "Iconizer: Moving catalog's json to '${CATALOG_PATH}/data.json'"
-
-# Copies the font file to the HTML directory
-cp -r ${FONT_PATH} ${CATALOG_PATH}/${FONT_NAME}
-echo "Iconizer: Moving catalog's font to '${CATALOG_PATH}/${FONT_NAME}'"
-}
-function init()
-{
-# Input variables
-FONT_PATH=$1
-echo "Iconizer: Initializing with font at path '${FONT_PATH}'"
 # Input's file name and extension
-FONT_NAME=$(basename "${FONT_PATH}")
-INPUT_EXTENSION="${FONT_NAME##*.}"
-echo "Iconizer: Processing file '${FONT_NAME}' with extension '${INPUT_EXTENSION}'"
-# Only TTF and OTF are supported font files
-if [ ${INPUT_EXTENSION} = 'ttf' ] || [ ${INPUT_EXTENSION} = 'otf' ]; then
-iconize ${FONT_PATH} ${FONT_NAME}
-else
-echo "Iconizer: Unsupported '${INPUT_EXTENSION}' file. Please provide a TTF or OTF file path."
+FILE_NAME=$(basename "${FILE_PATH}")
+FILE_EXTENSION="${FILE_NAME##*.}"
+FILE_TITLE="${FILE_NAME%.*}"
+
+ICON_NAME="${FILE_TITLE%%"-"*}Icon"
+
+# Capitalized first word of the file name, with the 'Icons' suffix.
+# ie: SlackIcons out of a string like 'slack-icon-font'
+OUTPUT_NAME=$(tr '[:lower:]' '[:upper:]' <<< ${ICON_NAME:0:1})${ICON_NAME:1}
+##Only TTF and OTF supported font files
+if [ -z ${FILE_PATH} ]; then
+echo "Missing file to parse. Please provide the path of a font file on disk (TTF or OTF)."
+elif [ ${FILE_EXTENSION} = 'ttf' ] || [ ${FILE_EXTENSION} = 'otf' ]; then
+OUTPUT_DIR=$2
+# When the out param is missing, reuse the same directory than the input's
+if [ -z ${OUTPUT_DIR} ] ; then
+OUTPUT_DIR=$(dirname ${FILE_PATH})
 fi
-}
-# Handle missing file path
-if [ -z ${INPUT_PATH} ]; then
-echo "Iconizer: No font file was found at path '${INPUT_PATH}'. Using FontAwesome as default font."
-# Uses FontAwesome as default
-init 'Fonts/FontAwesome/FontAwesome.otf'
+FONT_PATH=/Library/Fonts/${FILE_NAME}
+# Copies the original font to Library/Fonts if not already
+# The font file needs to be available before executing the SwiftGen template.
+if [ ! -f ${FONT_PATH} ]; then
+cp -r ${FILE_PATH} ${FONT_PATH}
+fi
+# Declares the output path
+OUTPUT_PATH=${OUTPUT_DIR}
+
+# Creates the output folder
+mkdir -p ${OUTPUT_PATH}
+
+# Executes Swiftgen with a custom stencil template
+${EXEC_PATH} icons ${FONT_PATH} --templatePath ${STENCIL_PATH} --output ${OUTPUT_PATH}${OUTPUT_NAME}.swift --enumName ${OUTPUT_NAME}
+# Renames and moves the JSON output to the HTML source directory
+cp -r ${OUTPUT_PATH}${OUTPUT_NAME}.json ${HTML_DIR}/html/data.json
+# Copies the HTML template over
+cp -r ${HTML_DIR}/* ${OUTPUT_PATH}
+
+# Copies the original font to src/
+cp -r ${FONT_PATH} ${OUTPUT_PATH}${FILE_NAME}
+cp -r ${FONT_PATH} ${HTML_DIR}/html/${FILE_NAME}
+
 else
-init ${INPUT_PATH}
+echo "Please provide a TTF or OTF file."
 fi
